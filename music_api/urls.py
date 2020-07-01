4469b9e581b61ae3ff96ed440b94052fb2624214
Millks <misaar@yandex.ru>
2020-07-01 21:04:04 +0300

    first views' tests

--- music_api/urls.py
+++ music_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import MelodyView
-app_name = "articles"
+app_name = ""
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('melodys/', MelodyView.as_view()),
+    path('melodys/', MelodyView.as_view(), name="melodys"),
 ]
